52c190e2531668fe3abfddcf2390d747ed39144f
awsajay19may <137310012+awsajay19may@users.noreply.github.com>
2023-06-22 15:58:26 +0100

    Update streamlit_app.py
    added menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 # created the main python file
 import streamlit 
 streamlit.title("My parents healthy dinner")
+streamlit.header("Breakfast Menu")
+streamlit.text("Omlette")
+streamlit.text("Fruits")
+streamlit.text("Oats")
